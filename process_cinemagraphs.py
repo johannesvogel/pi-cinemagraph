045c348ef4a62179503deb56c88d2fbe71f01a78
Johannes Vogel <johannesvogel@users.noreply.github.com>
2018-11-04 22:30:58 +0100

    resize and crop video files

--- process_cinemagraphs.py
+++ process_cinemagraphs.py
@@ -1,9 +1,13 @@
 import os
 import sys
 
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from time import sleep
+from moviepy.editor import VideoFileClip, vfx
+from decimal import Decimal
 
-FINAL_CLIP_LENGTH = 90  # seconds
+TARGET_CLIP_LENGTH = 90  # seconds
+TARGET_WIDTH = 1024
+TARGET_HEIGHT = 600
 
 def process_cinemagraphs():
   cinemagraphs = []
@@ -28,15 +32,48 @@ def process_cinemagraphs():
     print('({}/{}) Processing "{}".'.format(i+1, len(cinemagraphs), filename))
 
     clip = VideoFileClip(cinemagraph)
-    clip_length = clip.duration
-    loops = int(FINAL_CLIP_LENGTH / clip_length)
+    resolution = clip.size
+    clip.reader.close()
+    if hasattr(clip, 'audio') and hasattr(clip.audio, 'reader'):
+      clip.audio.reader.close_proc()
+    del clip
 
-    looped_clip = clip.loop(n=loops)
+    width = resolution[0]
+    height = resolution[1]
+    
+    scale = max(TARGET_WIDTH/width, TARGET_HEIGHT/height)
+
+    new_width = int(round(width*scale, 0))
+    new_height = int(round(height*scale, 0))
+
+    resized_clip = VideoFileClip(
+      cinemagraph,
+      target_resolution=(new_height, new_width),
+      resize_algorithm='bilinear'
+    )
+
+    x_center = int(round(new_width/2, 0))
+    y_center = int(round(new_height/2, 0))
+
+    cropped_clip = vfx.crop(
+      resized_clip, 
+      x_center=x_center , 
+      y_center=y_center, 
+      width=TARGET_WIDTH, 
+      height=TARGET_HEIGHT
+    )
+
+    clip_length = cropped_clip.duration
+    loops = int(TARGET_CLIP_LENGTH / clip_length)
+
+    looped_clip = cropped_clip.loop(n=loops)
     looped_clip.write_videofile(
       processed_path,
       audio=False,
       codec='mpeg4',
-      bitrate='2500k',
+      bitrate='5000k',
     )
 
   print('Processed {} new cinemagraph(s).'.format(len(cinemagraphs)))
+
+process_cinemagraphs()
